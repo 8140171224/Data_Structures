c67aa6a22934ae618758fa6a3e0926b36b853bfa
Aakash Padhiyar <45361041+8140171224@users.noreply.github.com>
2019-05-29 06:11:20 +0530

    Add files via upload

---
+++ Basic's/del_statement.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+# del
+a = [-1, 1, 66, 25, 67, 555, 1234, 5678.5]
+
+
+# In[15]:
+
+
+del a[0]
+
+
+# In[16]:
+
+
+a
+# -1 = index(0) removed
+
+
+# In[17]:
+
+
+print(a[2:4])
+del a[2:4]
+
+
+# In[18]:
+
+
+a
+# 25, 67 = index(2), index(3) are removed 
+
+
+# In[19]:
+
+
+del a[:]
+#empty
+
+
+# In[20]:
+
+
+a
+
+
+# In[21]:
+
+
+del a
+
+
+# In[23]:
+
+
+a
+# del a variable
+
+
+# In[ ]:
+
+
+# INFORMATION
+
+## BY aakashpadhiyar
+
+## BY 8140171224
+
