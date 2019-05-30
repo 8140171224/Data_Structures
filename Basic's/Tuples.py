66b748948a3304330b48aaad291bfa44e440f86e
Aakash Padhiyar <45361041+8140171224@users.noreply.github.com>
2019-05-30 06:43:52 +0530

    Add files via upload

---
+++ Basic's/Tuples.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Tuples & Sequences
+
+t = 123, 5342, 'hello'
+
+
+# In[2]:
+
+
+t[0]
+
+
+# In[3]:
+
+
+t
+
+
+# In[4]:
+
+
+u = t, (1, 2, 3, 4, 5, )
+
+
+# In[5]:
+
+
+u
+
+
+# In[7]:
+
+
+# Tuples are immutable
+t[0] = 8888
+
+
+# In[8]:
+
+
+v = ([1, 2, 3], [3, 2, 1])
+
+
+# In[9]:
+
+
+v
+
+
+# In[10]:
+
+
+#NEXT
+
+
+# In[11]:
+
+
+empty = ()
+Singleton = 'hello',
+
+
+# In[12]:
+
+
+len(empty)
+
+
+# In[14]:
+
+
+len(Singleton)
+
+
+# In[15]:
+
+
+Singleton
+
+
+# In[ ]:
+
+
+#INFORMATION
+## BY aakashpadhiyar
+## BT 8140171224
+
