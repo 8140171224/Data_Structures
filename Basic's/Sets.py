9978231c7a79658fbcd643df16509afc95fe07f2
Aakash Padhiyar <45361041+8140171224@users.noreply.github.com>
2019-05-30 06:59:14 +0530

    Add files via upload

---
+++ Basic's/Sets.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Sets
+
+basket = {'apple', 'banana', 'orange', 'apple', 'pear', 'orange'}
+
+
+# In[2]:
+
+
+basket
+
+
+# In[3]:
+
+
+'apple' in basket
+
+
+# In[4]:
+
+
+'crabgrass' in basket
+
+
+# In[5]:
+
+
+#little High e.g
+
+
+# In[6]:
+
+
+a = set('abracadabra')
+
+
+# In[7]:
+
+
+b = set('alacazam')
+
+
+# In[8]:
+
+
+a | b
+
+
+# In[9]:
+
+
+a & b
+
+
+# In[10]:
+
+
+a ^ b
+
+
+# In[11]:
+
+
+#SET & loop
+
+
+# In[12]:
+
+
+a = {x for x in 'abracadabra' if x not in 'abc'}
+
+
+# In[13]:
+
+
+a
+
+
+# In[ ]:
+
+
+# INFORMATION
+## BY aakashpadhiyar
+## BY 814071224
+
