98b9a9d56a2eb984bac4fc56325617dc893ea66f
Aakash Padhiyar <45361041+8140171224@users.noreply.github.com>
2019-05-31 05:50:52 +0530

    Add files via upload

---
+++ Basic's/Dictionaries.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#DICTIONARIES
+
+tel = {'aakash': 1281, 'Dhaval': 1094}
+
+
+# In[7]:
+
+
+tel['ashok'] = 3168
+
+
+# In[8]:
+
+
+tel['aakash']
+
+
+# In[9]:
+
+
+del tel['ashok']
+
+
+# In[11]:
+
+
+tel['yogita']  = 4844
+
+
+# In[12]:
+
+
+tel
+
+
+# In[13]:
+
+
+list(tel)
+
+
+# In[14]:
+
+
+sorted(tel)
+
+
+# In[15]:
+
+
+'aakash' in tel
+
+
+# In[16]:
+
+
+'ashok' in tel
+
+
+# In[17]:
+
+
+#NEXT
+
+
+# In[21]:
+
+
+dict([('aakash', 1284), ('dhaval', 1281), ('yogita', 4844)])
+
+
+# In[22]:
+
+
+#NEXT
+
+
+# In[23]:
+
+
+{x: x**2 for x in (2, 4, 6)}
+
+
+# In[24]:
+
+
+#NEXT
+
+
+# In[25]:
+
+
+dict(aakash=1281, dhaval=1094, yogita=4844)
+
+
+# In[ ]:
+
+
+# INFORMATION
+
+## BY aakashpadhiyar
+## BY 8140171224
+
